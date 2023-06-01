76ae47c5a9261cac9d2767d169bf4f511d4ed96e
Mike Lee <programmablemike@gmail.com>
2023-06-01 10:39:55 -0700

    Updating the code to perform TCP echoing

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM golang:1.16.3-alpine3.13 AS builder
 WORKDIR /go/src/github.com/codeteam2-dev/echo
 COPY . .
+RUN go mod download
 RUN go build -o /go/bin/echo
 
 FROM scratch
--- main.go
+++ main.go
@@ -1,9 +1,33 @@
+// a simple TCP echo server
+// this code was lifted from the official Go documentation at https://pkg.go.dev/net#example-Listener
 package main
 
 import (
-	"fmt"
+	"io"
+	"net"
+
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Info().Msg("Starting echo server...")
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to start server")
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Error().Err(err).Msgf("Failed to accept connection")
+			continue
+		}
+		log.Info().Msgf("Accepted connection from %s", conn.RemoteAddr())
+		go func(conn net.Conn) {
+			// Echo all incoming data.
+			io.Copy(conn, conn)
+			conn.Close()
+
+		}(conn)
+	}
 }
